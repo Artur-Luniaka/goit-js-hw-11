f622dfb3a47289b33748b2d1485b01b2aad94e27
Artur Luniaka <artur01049718@gmail.com>
2024-09-13 08:38:07 +0100

    HW-11 in progress

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { fetchImages } from './js/pixabay-api';
+
+export let userQuery;
+
+const userForm = document.querySelector('.form');
+
+const handleImages = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    userQuery = form.elements.query.value;
+    fetchImages(userQuery);
+    form.reset();
+};
+
+userForm.addEventListener('submit', handleImages);
 
