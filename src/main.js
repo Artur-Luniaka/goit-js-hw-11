1554bddd38a8991d4f1a1db9a29527b65b08b557
Artur Luniaka <artur01049718@gmail.com>
2024-09-15 08:40:06 +0100

    finish HW-11

--- src/main.js
+++ src/main.js
@@ -1,62 +1,50 @@
+import { fetchImages } from "./js/pixabay-api";
+import { renderImages } from "./js/render-functions";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const userForm = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const loader = () => document.querySelector("span").classList.toggle("loader");
 
-let userQuery;
+const warn = () => iziToast.show({
+    message: 'Please fill a field',
+    backgroundColor: 'red',
+    color: 'white',
+});
+
+const mistake = () => iziToast.show({
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    backgroundColor: 'red',
+    color: 'white',
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
 
 const handleImages = (event) => {
     event.preventDefault();
-    const form = event.target;
-    userQuery = form.elements.query.value.trim();
-    if (userQuery === "") {
-        iziToast.show({
-            message: 'Fill enter field',
-            backgroundColor: 'red',
-            color: 'white',
-        })
-    }
+    const userQuery = event.currentTarget.elements.query.value.toLowerCase().trim();
+    if (!userQuery) return warn();
+        gallery.innerHTML = "";
+        loader();
     fetchImages(userQuery)
-    form.reset();
-};
-
-const fetchImages = (userQuery) => {
-    const searchImages = new URLSearchParams({
-        key: "45941098-20d6b06978404a6b7356e16b7",
-        q: userQuery,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: "true",
-    });
-
-    if (userQuery === '') {
-        return;
-    }
-
-    const urlSearch = `https://pixabay.com/api/?${searchImages}`;
-
-    fetch(urlSearch)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error();
-            }
-            return response.json();
+        .then((json) => {
+            if (!json.hits.length) return mistake();
+                gallery.insertAdjacentHTML('beforeend', renderImages(json.hits).join(''));
+                lightbox.refresh();
         })
-        .then(data => {
-            if (data.hits.length === 0) {
-                iziToast.show({
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    backgroundColor: 'red',
-                    color: 'white'
-                })
-            }
-            console.log(data);
+        .catch((error) => {
+            console.log(error);
         })
-        .catch(error => {
-            console.error(error);
-        });
-}
+        .finally(() => {loader()});
+    userForm.reset();
+    };
 
 userForm.addEventListener('submit', handleImages);
